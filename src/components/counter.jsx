a1d3d48c5b12e2581aa50e9e6acb4824abdda9f8
Chelsie Conrad <chelsiechey@Chelsies-MacBook-Air.local>
2021-05-03 16:03:44 -0600

    first commit

---
+++ src/components/counter.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+class Counter extends Component {
+    // state = {  }
+    render() { 
+        return <h1>Hello World</h1>; // when rendered returns an h1 element
+    }
+}
+ 
+export default Counter; // can also be written in the class declaration
